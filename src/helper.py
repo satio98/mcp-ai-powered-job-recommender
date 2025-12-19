0edd4c7d897026c1093bdd2f8315fc2a145e923c
satio98 <satio98@gmail.com>
2025-12-19 19:47:06 +0700

    Modified helper functions.

--- src/helper.py
+++ src/helper.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from apify_client import ApifyClient
 
-load_dotenv()a
+load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
