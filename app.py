ca0b6ec7fa27700ba6c95e167d1e73eb942c66d2
satio98 <satio98@gmail.com>
2025-12-19 20:08:11 +0700

    Established streamlit framework.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from job!")
+import streamlit as st
+from src.helper import extract_text_from_pdf
 
+uploaded_file = st.file_uploader('Upload', type = 'pdf')
+
+if uploaded_file:
+    st.markdown(extract_text_from_pdf(uploaded_file))
 
 if __name__ == "__main__":
-    main()
+    pass
